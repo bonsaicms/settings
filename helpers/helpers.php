2b178a49a714303c31748e69ee7a977d10c3311b
mspiderv <mspiderv@gmail.com>
2020-11-11 00:41:58 +0100

    smarter helper

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,20 +1,32 @@
 <?php
 
+use Illuminate\Support\Collection;
 use BonsaiCms\Settings\Contracts\SettingsManager;
 
 if ( ! function_exists('settings')) {
-    function settings($mixed = null, $value = null)
+    function settings(...$params)
     {
         $settings = app(SettingsManager::class);
 
-        if ($mixed === null) {
+        $paramsCount = count($params);
+
+        if ($paramsCount === 0) {
             return $settings;
         }
 
-        if (is_array($mixed) || $value !== null) {
-            return $settings->set($mixed, $value);
+        if ($paramsCount === 1) {
+            if (is_array($params[0])) {
+                $ar = new Collection($params[0]);
+                if ($ar->keys()->join('') === $ar->values()->keys()->join('')) {
+                    return $settings->get($params[0]);
+                } else {
+                    return $settings->set($params[0]);
+                }
+            } else {
+                return $settings->get($params[0]);
+            }
         }
 
-        return $settings->get($mixed);
+        return $settings->set($params[0], $params[1]);
     }
 }
