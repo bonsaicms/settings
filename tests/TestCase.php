6fd44fb876e064f38eae4363dcecb1ed113d3a69
mspiderv <mspiderv@gmail.com>
2020-11-11 17:03:57 +0100

    tests fixed

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,19 +7,6 @@ use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('settings', [
-            'autoload' => false,
-        ]);
-    }
-
     protected function toArray($mixed)
     {
         return ($mixed instanceof Collection)
